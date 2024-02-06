d98e5f8448971287eaefd9b550c23b2a6f7e66fe
gyeongsu <choiiisu11@gmail.com>
2024-02-06 18:49:10 +0900

    feat: JwtProvider, Jwt 클래스 구현
    signed JWT를 생성하는 JwtProvider 클래스와 accessToken과 refreshToken을 담고 있는 Jwt 클래스를 구현하였습니다.

---
+++ BE/iDrop/src/main/java/ifive/idrop/jwt/JwtProvider.java
@@ -0,0 +1,51 @@
+package ifive.idrop.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtProvider {
+    public static final byte[] secret = "IDropSecretKeyIDropSecretKeyIDropSecretKey".getBytes();
+    private final SecretKey secretKey = Keys.hmacShaKeyFor(secret);
+
+    public String createToken(Map<String, Object> claims, Date expireDate) {
+        return Jwts.builder()
+                .claims(claims)
+                .expiration(expireDate)
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public Jwt createJwt(Map<String, Object> claims) {
+        String accessToken = createToken(claims, getExpireDateAccessToken());
+        String refreshToken = createToken(new HashMap<>(), getExpireDateRefreshToken());
+        return Jwt.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+    public Date getExpireDateAccessToken() {
+        long expireTimeMils = 1000 * 60 * 60;
+        return new Date(System.currentTimeMillis() + expireTimeMils);
+    }
+
+    public Date getExpireDateRefreshToken() {
+        long expireTimeMils = 1000L * 60 * 60 * 24 * 60;
+        return new Date(System.currentTimeMillis() + expireTimeMils);
+    }
+}

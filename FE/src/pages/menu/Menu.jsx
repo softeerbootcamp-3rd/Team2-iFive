d31ccfd0a3b7ef2366968a14e95de0a9e7510a99
육종호 <dbrwhdgh1203@naver.com>
2024-02-07 18:42:25 +0900

    feat: 메뉴 페이지 구현

---
+++ FE/src/pages/menu/Menu.jsx
@@ -0,0 +1,33 @@
+import styles from "./Menu.module.scss";
+import { MenuButton } from "./menuButton";
+import Location from "@/assets/Location.svg";
+import iDropGreen from "@/assets/iDropGreen.svg";
+import Star from "@/assets/Star.svg";
+import Success from "@/assets/Success.svg";
+import User from "@/assets/User.svg";
+import { BottomSheet } from "../../Components/Common/Bottomsheet/Bottomsheet";
+
+export function Menu() {
+    const data = {
+        name: "육 아들",
+        time: "7:00~8:00",
+        start: "서울 시청",
+        goal: "국민대"
+    };
+    return (
+        <div className={styles.wrapper}>
+            <img src={iDropGreen}></img>
+            <div>
+                <h1>안녕하세요, 육종호님!</h1>
+                <span>오늘도 안전하게 픽업할게요</span>
+            </div>
+            <div className={styles.menuContainer}>
+                <MenuButton imgUrl={Location} text="실시간 위치"></MenuButton>
+                <MenuButton imgUrl={Success} text="구독하기"></MenuButton>
+                <MenuButton imgUrl={User} text="프로필"></MenuButton>
+                <MenuButton imgUrl={Star} text="이용내역"></MenuButton>
+            </div>
+            <BottomSheet data={data}></BottomSheet>
+        </div>
+    );
+}

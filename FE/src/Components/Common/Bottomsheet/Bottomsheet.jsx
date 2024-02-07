14d1ada72a32c52f25c9d4163992ebe28936243f
육종호 <dbrwhdgh1203@naver.com>
2024-02-07 18:44:37 +0900

    style: 바텀시트 클릭 이벤트 추가 및 스타일 변경

--- FE/src/Components/Common/Bottomsheet/Bottomsheet.jsx
+++ FE/src/Components/Common/Bottomsheet/Bottomsheet.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
 import styles from "./bottomsheet.module.scss";
-import { KidInfo } from "./KidInfoBox";
+import { KidInfo } from "./kidInfoBox";
 export function BottomSheet({ data }) {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const toggleBottomSheet = () => {
+        setIsExpanded(!isExpanded);
+    };
+
     return (
-        <div className={styles.wrapper}>
+        <div
+            onClick={toggleBottomSheet}
+            className={`${styles.wrapper} ${isExpanded ? styles.expanded : ""}`}
+        >
+            <header>
+                <div className={styles.handle}></div>
+            </header>
             <KidInfo data={data}></KidInfo>
         </div>
     );

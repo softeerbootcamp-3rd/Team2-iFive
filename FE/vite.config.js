e4123fb0bb341fb24c16a51f0f47b0011a8a8acc
육종호 <dbrwhdgh1203@naver.com>
2024-02-07 11:41:21 +0900

    chore: plugin 라이브러리 설치 및 설정

--- FE/vite.config.js
+++ FE/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import { createHtmlPlugin } from "vite-plugin-html";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+    console.log(env); // 터미널에 선언한 환경변수들이 보인다.
+    return {
+        plugins: [
+            react(),
+            createHtmlPlugin({
+                minify: true,
+                inject: {
+                    data: {
+                        naverApiKey: env.VITE_NAVER_KEY
+                    }
+                }
+            })
+        ]
+    };
+});

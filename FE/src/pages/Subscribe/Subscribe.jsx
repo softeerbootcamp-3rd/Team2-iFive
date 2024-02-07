70b41540728fa2f843b721358467f10bd12e867d
Sang-minKIM <kimpran@naver.com>
2024-02-07 11:04:45 +0900

    refactor: 컴포넌트 반복 줄임

--- FE/src/pages/Subscribe/Subscribe.jsx
+++ FE/src/pages/Subscribe/Subscribe.jsx
@@ -1,32 +1,55 @@
 import { useState } from "react";
 import styles from "./Subscribe.module.scss";
-import backIcon from "@/assets/back.svg";
+
+import { Header } from "@/components/common/Header/Header";
+import { Footer } from "@/components/common/Footer/Footer";
+
+const weekDays = [
+    "일요일",
+    "월요일",
+    "화요일",
+    "수요일",
+    "목요일",
+    "금요일",
+    "토요일"
+];
+
+function TimeItem({ day }) {
+    return (
+        <li className={styles.timeItem}>
+            <h6 className={styles.timeDay}>{day}</h6>
+            <form className={styles.timeForm}>
+                <div className={styles.timeWrapper}>
+                    <input type="text" className={styles.timeInput} />
+                    <label className={styles.timeSuffix}>시</label>
+                    <input type="text" className={styles.timeInput} />
+                    <label className={styles.timeSuffix}>분</label>
+                </div>
+            </form>
+        </li>
+    );
+}
 
 export default function Subscribe() {
     const [timeList, setTimeList] = useState(["일요일"]);
+
+    const dayListElement = weekDays.map((day) => (
+        <li key={day} className={styles.dayItem}>
+            {day[0]}
+        </li>
+    ));
+
     const timeListElement = timeList.length ? (
-        timeList.map((day) => (
-            <li className={styles.timeItem}>
-                <h6 className={styles.timeDay}>{day}</h6>
-                <form className={styles.timeForm}>
-                    <div className={styles.timeWrapper}>
-                        <input type="text" className={styles.timeInput} />
-                        <label className={styles.timeSuffix}>시</label>
-                        <input type="text" className={styles.timeInput} />
-                        <label className={styles.timeSuffix}>분</label>
-                    </div>
-                </form>
-            </li>
+        timeList.map((day, index) => (
+            <TimeItem day={day} key={`day-${index}`} />
         ))
     ) : (
         <li className={styles.timeEmpty}>픽업 요일을 선택해주세요</li>
     );
+
     return (
         <main className={styles.container}>
-            <header className={styles.header}>
-                <img src={backIcon} />
-                <h2 className={styles.title}>픽업 신청</h2>
-            </header>
+            <Header title="픽업 신청" />
             <section className={styles.contents}>
                 <form className={styles.locationForm}>
                     <label className={styles.locationLabel}>
@@ -43,28 +66,16 @@ export default function Subscribe() {
                         placeholder="도착지"
                     />
                 </form>
-                <form className={styles.dayForm}>
+                <article className={styles.dayBox}>
                     <label className={styles.dayLabel}>픽업 요일</label>
-                    <ul className={styles.dayList}>
-                        <li className={styles.dayItem}>일</li>
-                        <li className={styles.dayItem}>월</li>
-                        <li className={styles.dayItem}>화</li>
-                        <li className={styles.dayItem}>수</li>
-                        <li className={styles.dayItem}>목</li>
-                        <li className={styles.dayItem}>금</li>
-                        <li className={styles.dayItem}>토</li>
-                    </ul>
-                </form>
-                <form className={styles.time}>
+                    <ul className={styles.dayList}>{dayListElement}</ul>
+                </article>
+                <article className={styles.time}>
                     <label className={styles.timeLabel}>픽업 시간</label>
                     <ul className={styles.timeList}>{timeListElement}</ul>
-                </form>
+                </article>
             </section>
-            <footer className={styles.footer}>
-                <button className={styles.footerBtn} type="button">
-                    확인
-                </button>
-            </footer>
+            <Footer text="확인" />
         </main>
     );
 }

d598ef190a46679919eed17c0ad2b24f58c562ec
육종호 <dbrwhdgh1203@naver.com>
2024-02-07 11:38:46 +0900

    chore: Map 페이지 라우터  추가

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Map from "./pages/Map/Map";
 import Home from "./pages/Home";
 // 페이지 만들 때 마다 주석 제거할 예정
 function App() {

c296c31226ee67eb812efb446e3e2df0ad0f87b1
육종호 <dbrwhdgh1203@naver.com>
2024-02-07 18:43:53 +0900

    style: 아이정보 박스 스타일 변경

--- FE/src/Components/Common/Bottomsheet/KidInfoBox.jsx
+++ FE/src/Components/Common/Bottomsheet/KidInfoBox.jsx
@@ -3,9 +3,6 @@ import styles from "./kidInfoBox.module.scss";
 export function KidInfo({ data: { name, time, start, goal } }) {
     return (
         <>
-            <header>
-                <div className={styles.handle}></div>
-            </header>
             <div className={styles.content}>
                 <div className={styles.kidImg}>아이사진</div>
                 <div className={styles.infoBox}>

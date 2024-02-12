fa8145146f36001ad9d72e8e588c0fcfa48a0487
Sang-minKIM <kimpran@naver.com>
2024-02-12 13:15:09 +0900

    feat: 주소를 좌표로 바꾸는 함수 작성

--- FE/src/components/Search/AddressFinderMap/initMap.js
+++ FE/src/components/Search/AddressFinderMap/initMap.js
@@ -1,5 +1,49 @@
 const DEFAULT_ZOOM_SCALE = 15;
 
+const searchAddressToCoordinate = (address) => {
+    naver.maps.Service.geocode(
+        {
+            query: address
+        },
+        (status, response) => {
+            if (status === naver.maps.Service.Status.ERROR) {
+                return alert("Something Wrong!");
+            }
+
+            // if (response.v2.meta.totalCount === 0) {
+            //     return alert(`totalCount ${response.v2.meta.totalCount}`);
+            // }
+
+            const item = response.v2.addresses[0];
+            const point = new naver.maps.Point(item.x, item.y);
+            const htmlAddresses = [];
+
+            if (item.roadAddress) {
+                htmlAddresses.push(`[도로명 주소] ${item.roadAddress}`);
+            }
+
+            if (item.jibunAddress) {
+                htmlAddresses.push(`[지번 주소] ${item.jibunAddress}`);
+            }
+
+            if (item.englishAddress) {
+                htmlAddresses.push(`[영문명 주소] ${item.englishAddress}`);
+            }
+
+            infoWindow.setContent(
+                [
+                    `<div style="padding:10px;min-width:200px;line-height:150%;">`,
+                    `<h4 style="margin-top:5px;">검색 주소 : ${address}</h4><br />`,
+                    htmlAddresses.join("<br />"),
+                    `</div>`
+                ].join("\n")
+            );
+
+            map.setCenter(point);
+        }
+    );
+};
+
 function searchCoordinateToAddress(latLng, setNewAddress) {
     naver.maps.Service.reverseGeocode(
         {

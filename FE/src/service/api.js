3c88304bcaf0e53b59f3793f2d3b88acff380ca5
Sang-minKIM <kimpran@naver.com>
2024-02-12 15:47:03 +0900

    feat: auth 필요한 요청 로직 구현

--- FE/src/service/api.js
+++ FE/src/service/api.js
@@ -1,3 +1,6 @@
+import { logout } from "../pages/Login/Login";
+import { getAccessToken, getRefreshToken } from "../utils/getTokens";
+
 /**
  * @param {Object} props
  * @param {string} props.id 사용자 ID
@@ -29,5 +32,67 @@ export async function login({ id, password }) {
     } catch (error) {
         console.error("Login error:", error);
         return { success: false, message: error.toString() };
+    }
+}
+
+// TODO - 배포 주소로 변경
+async function refreshAccessToken(refreshToken) {
+    const response = await fetch("/api/refresh_token", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ refreshToken })
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem("accessToken", data.accessToken);
+        localStorage.setItem("refreshToken", data.refreshToken);
+        return data.accessToken;
+    } else {
+        throw new Error("Refresh token is invalid or expired.");
+    }
+}
+
+async function sendAuthRequest(url, options = {}) {
+    const accessToken = getAccessToken();
+    if (accessToken) {
+        options.headers = {
+            ...options.headers,
+            Authorization: `Bearer ${accessToken}`
+        };
+    }
+
+    let response = await fetch(url, options);
+
+    // TODO - 액세스 토큰이 만료되었을 경우 로직, status 코드 백엔드에 따라 변경 필요
+    if (response.status === 401) {
+        try {
+            const newAccessToken = await refreshAccessToken();
+            options.headers.Authorization = `Bearer ${newAccessToken}`;
+            response = await fetch(url, options);
+        } catch (error) {
+            console.error("Session expired. User needs to login again.");
+            logout();
+        }
+    }
+
+    return response;
+}
+// 위의 두 함수 사용 예시.
+async function submitReview(reviewData) {
+    try {
+        const response = await sendRequest("/api/reviews", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(reviewData)
+        });
+
+        if (response.ok) {
+            console.log("Review submitted successfully.");
+        } else {
+            console.error("Failed to submit review.");
+        }
+    } catch (error) {
+        console.error("Error submitting review:", error);
     }
 }

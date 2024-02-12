b7309dcf673fa02a8ac48006f30e5cc3d8b3d235
win9 <krrr883@gmail.com>
2024-02-13 02:14:51 +0900

    feat: 예약시간필드 타입 변경
    시간이 아닌 크론식으로 변경하여 원자성을 지키도록 구형

--- BE/iDrop/src/main/java/ifive/idrop/entity/PickUp.java
+++ BE/iDrop/src/main/java/ifive/idrop/entity/PickUp.java
@@ -1,11 +1,15 @@
 package ifive.idrop.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
+@Builder
+@RequiredArgsConstructor
 public class PickUp {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,9 +20,14 @@ public class PickUp {
     private String endImage;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
-    private LocalDateTime reservedTime;
+    private String reservedTime; // cron 식으로 변경
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "pickup_subscribe_id")
     private PickUpSubscribe pickUpSubscribe;
+
+    public void updatePickUpSubscribe(PickUpSubscribe subscribe) {
+        this.pickUpSubscribe = subscribe;
+        subscribe.getPickUpList().add(this);
+    }
 }

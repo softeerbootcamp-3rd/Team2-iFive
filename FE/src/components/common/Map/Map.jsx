235ddb679dac60f16ff22e4633f0b08d7ff9155a
육종호 <dbrwhdgh1203@naver.com>
2024-02-13 14:00:29 +0900

    feat: map 생성, 마크 생성 로직 util로 분리

--- FE/src/components/common/Map/Map.jsx
+++ FE/src/components/common/Map/Map.jsx
@@ -1,17 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import styles from "../Map/map.module.scss";
-
-export const initMapOptions = {
-    mapDataControl: false,
-    scaleControl: true,
-    scaleControlOptions: {
-        position: naver.maps.Position.TOP_RIGHT
-    },
-    logoControl: true,
-    logoControlOptions: {
-        position: naver.maps.Position.TOP_LEFT
-    }
-};
+import { generateMap, generateMarker, getLatLng } from "../../../utils/map";
 
 const mockLocations = [
     { x: 37.56516609048783, y: 126.97715864963087 },
@@ -21,37 +10,33 @@ const mockLocations = [
 
 const createMarker = (locations, map) => {
     locations.forEach((location, index) => {
-        new naver.maps.Marker({
-            position: new naver.maps.LatLng(location.x, location.y),
-            map: map
-        });
+        const position = getLatLng(location.x, location.y);
+        generateMarker(map, position);
     });
 };
 
 export default function Map({
     locations = mockLocations,
     zoom = 13,
-    width = "100%",
-    height = "100%",
     addOption = {}
 }) {
+    const mapRef = useRef();
     const customMapOptions = {
-        ...initMapOptions,
         center: new naver.maps.LatLng(mockLocations[1].x, mockLocations[1].y),
         zoom: zoom,
         ...addOption
     };
 
     useEffect(() => {
-        const mapDiv = document.getElementById("map");
-        const map = new window.naver.maps.Map(mapDiv, customMapOptions);
+        const mapDiv = mapRef.current;
+        const map = generateMap(mapDiv, customMapOptions);
         // TO DO: 출발지 목적지 좌표값 받아오기
         createMarker(locations, map);
     }, [locations]);
 
     return (
         <div className={styles.wrapper}>
-            <div id="map" style={{ width, height }} />
+            <div ref={mapRef} className={styles.map} />
         </div>
     );
 }

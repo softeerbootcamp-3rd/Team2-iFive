37101dd52349c32bc631205c6a3bd36aadf73d29
Sang-minKIM <kimpran@naver.com>
2024-02-13 17:10:35 +0900

    fix: data router로 변경

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+    Route,
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements
+} from "react-router-dom";
 import Search from "./pages/Search/Search";
 import Onboarding from "./pages/Onboarding/Onboarding";
 import Login, { logout as logoutAction } from "./pages/Login/Login";
@@ -10,41 +15,40 @@ import { checkAuthLoader } from "./utils/auth";
 import { Locate } from "./pages/Locate/Locate";
 import { PickUpPage } from "./pages/PickUp/PickUp";
 
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route>
+            <Route path="/" element={<Menu />} />
+            <Route path="onboarding" element={<Onboarding />} />
+            <Route path="login" element={<Login />} />
+            <Route path="logout" action={logoutAction} />
+            {/* <Route path="join" element={<Join />} /> */}
+            {/* 로그인 필요한 페이지 예시   <Route path="subscription/form" loader={checkAuthLoader} element={<Subscribe />} /> */}
+            <Route path="subscription/search" element={<Search />} />
+            <Route path="subscription/drivers" element={<DriverList />} />
+            {/* <Route
+            path="subscription/driver/:id"
+            element={<DriverDetail />}
+        /> */}
+
+            {/*<Route path="kid" element={<Kid />} /> */}
+            <Route path="map" element={<Locate />} />
+            {/* <Route path="feedback" element={<Feedback />} />
+        <Route path="pickup">
+            <Route path="start" element={<PickupStart />} />
+            <Route path="end" element={<PickupEnd />} />
+        </Route>
+        <Route path="driver-profile" element={<DriverProfile />} />
+        <Route path="call-list" element={<CallList />} /> */}
+            <Route path="publish" element={<Publish />}></Route>
+            <Route path="pickup" element={<PickUpPage />}></Route>
+        </Route>
+    )
+);
+
 // 페이지 만들 때 마다 주석 제거할 예정
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Menu />} />
-                <Route path="onboarding" element={<Onboarding />} />
-                <Route path="login" element={<Login />} />
-                <Route path="logout" action={logoutAction} />
-
-                {/* <Route path="join" element={<Join />} /> */}
-
-                {/* 로그인 필요한 페이지 예시
-                <Route path="subscription/form" loader={checkAuthLoader} element={<Subscribe />} /> */}
-                <Route path="subscription/search" element={<Search />} />
-                <Route path="subscription/drivers" element={<DriverList />} />
-                {/* <Route
-                    path="subscription/driver/:id"
-                    element={<DriverDetail />}
-                /> */}
-
-                {/*<Route path="kid" element={<Kid />} /> */}
-                <Route path="map" element={<Locate />} />
-                {/* <Route path="feedback" element={<Feedback />} />
-                <Route path="pickup">
-                    <Route path="start" element={<PickupStart />} />
-                    <Route path="end" element={<PickupEnd />} />
-                </Route>
-                <Route path="driver-profile" element={<DriverProfile />} />
-                <Route path="call-list" element={<CallList />} /> */}
-                <Route path="publish" element={<Publish />}></Route>
-                <Route path="pickup" element={<PickUpPage />}></Route>
-            </Routes>
-        </Router>
-    );
+    return <RouterProvider router={router} />;
 }
 
 export default App;

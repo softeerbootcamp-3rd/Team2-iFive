7e00110a472f70f3920b20ef21ad92643b042b9b
win9 <krrr883@gmail.com>
2024-02-13 17:04:41 +0900

    feat: 구독요청 controller 구현
    구독 신청 정보 정보를 받을 수 있는 controller 생성

---
+++ BE/iDrop/src/main/java/ifive/idrop/controller/ParentController.java
@@ -0,0 +1,25 @@
+package ifive.idrop.controller;
+
+import ifive.idrop.annotation.Login;
+import ifive.idrop.dto.BaseResponse;
+import ifive.idrop.dto.SubscribeRequest;
+import ifive.idrop.entity.Parent;
+import ifive.idrop.service.ParentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/parent")
+public class ParentController {
+    private final ParentService parentService;
+
+    @PostMapping("/subscrib")
+    public BaseResponse<String> subscribDriver(@Login Parent parent, @RequestBody SubscribeRequest request) {
+        return parentService.createSubscribe(parent, request);
+    }
+}

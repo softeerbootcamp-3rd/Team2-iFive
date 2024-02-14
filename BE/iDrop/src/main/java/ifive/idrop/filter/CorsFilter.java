f08b2de4406c816dacdbf31c3a322df1803cd9c6
win9 <krrr883@gmail.com>
2024-02-14 12:13:23 +0900

    feat: filter를 통해 header의 origin을 필터링 하도록 수정
    filter로직에서 설정한 origin이 매칭이 되어야 허용하도록 변경

--- BE/iDrop/src/main/java/ifive/idrop/filter/CorsFilter.java
+++ BE/iDrop/src/main/java/ifive/idrop/filter/CorsFilter.java
@@ -1,20 +1,33 @@
 package ifive.idrop.filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
 
 @RequiredArgsConstructor
+@Configuration
+@Slf4j
 public class CorsFilter implements Filter {
+
+    final static String requestOrigin = "http://localhost:5173";
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String origin = request.getHeader("Origin");
+
+        log.info("request origin = {}", origin);
+        if (requestOrigin.contains(origin)) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        }
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "*");
         chain.doFilter(servletRequest, response);
     }
 }

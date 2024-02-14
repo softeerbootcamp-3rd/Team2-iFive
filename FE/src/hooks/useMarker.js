79fdc644801965be70cd21342ea83767cabd88dc
Sang-minKIM <kimpran@naver.com>
2024-02-14 11:42:02 +0900

    feat: useMarker hook 구현

--- FE/src/hooks/useMarker.js
+++ FE/src/hooks/useMarker.js
@@ -1,15 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export function useMarker(map, position, options = {}) {
+    const [marker, setMarker] = useState(null);
     useEffect(() => {
-        if (!map || !position) return;
-
-        const marker = new naver.maps.Marker({
+        if (!map || !position) {
+            return;
+        }
+        const newMarker = new naver.maps.Marker({
             map,
             position,
             ...options
         });
-
+        setMarker(newMarker);
         return () => marker.setMap(null);
-    }, [map, position, options]);
+    }, [map]);
+
+    return marker;
 }

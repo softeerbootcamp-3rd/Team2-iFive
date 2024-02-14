778b43d2e9ec13d0d5ff4c66576f5198b0b20e6e
eekrwl <wani9909@naver.com>
2024-02-14 17:30:10 +0900

    chore: getter 추가

--- BE/iDrop/src/main/java/ifive/idrop/entity/PickUpInfo.java
+++ BE/iDrop/src/main/java/ifive/idrop/entity/PickUpInfo.java
@@ -1,11 +1,13 @@
 package ifive.idrop.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
 public class PickUpInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

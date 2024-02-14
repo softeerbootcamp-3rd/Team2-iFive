f307308d564d256ec286dcba9b3ad81c316a59d9
eekrwl <wani9909@naver.com>
2024-02-14 17:41:11 +0900

    feat: childInfo로 pickupInfo 조회
    아이 한명이 가지고 있는 만료된/만료전 픽업 모두 조회

---
+++ BE/iDrop/src/main/java/ifive/idrop/repository/PickUpRepository.java
@@ -0,0 +1,25 @@
+package ifive.idrop.repository;
+
+import ifive.idrop.entity.PickUpInfo;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class PickUpRepository {
+
+    private final EntityManager em;
+
+    public List<PickUpInfo> findPickupInfosByChildId(Long childId) {
+        TypedQuery<PickUpInfo> query = em.createQuery(
+                "SELECT pi FROM PickUpInfo  pi " +
+                        "WHERE pi.child.id = :childId ", PickUpInfo.class);
+        query.setParameter("childId", childId);
+
+        return query.getResultList();
+    }
+}

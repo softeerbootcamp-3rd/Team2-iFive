90ed91efef46fde4dff8533180007c285e6d7829
eekrwl <wani9909@naver.com>
2024-02-15 17:57:52 +0900

    feat: findByDriverId 메서드 추가

--- BE/iDrop/src/main/java/ifive/idrop/repository/DriverRepository.java
+++ BE/iDrop/src/main/java/ifive/idrop/repository/DriverRepository.java
@@ -1,2 +1,19 @@
-package ifive.idrop.repository;public class DriverRepository {
+package ifive.idrop.repository;
+
+import ifive.idrop.entity.Driver;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class DriverRepository {
+    private final EntityManager em;
+
+    public Optional<Driver> findByDriverId(Long driverId) {
+        Driver driver = em.find(Driver.class, driverId);
+        return Optional.ofNullable(driver);
+    }
 }

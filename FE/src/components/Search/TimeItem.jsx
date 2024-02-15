fac89359ee6292d9b2124f6e6aac8b280bd10ca9
Sang-minKIM <kimpran@naver.com>
2024-02-15 16:02:52 +0900

    refactor: NumericSelector 분리

--- FE/src/components/Search/TimeItem.jsx
+++ FE/src/components/Search/TimeItem.jsx
@@ -1,4 +1,6 @@
+import { NumericSelector } from "../common/Input/NumericSelector";
 import styles from "./TimeItem.module.scss";
+import React from "react";
 
 export function TimeItem({ day }) {
     return (
@@ -6,10 +8,24 @@ export function TimeItem({ day }) {
             <h6 className={styles.timeDay}>{day}</h6>
             <form className={styles.timeForm}>
                 <div className={styles.timeWrapper}>
-                    <input type="text" className={styles.timeInput} />
-                    <label className={styles.timeSuffix}>시</label>
-                    <input type="text" className={styles.timeInput} />
-                    <label className={styles.timeSuffix}>분</label>
+                    <NumericSelector
+                        start={0}
+                        end={23}
+                        step={1}
+                        targetLength={2}
+                        padChar="0"
+                        unit="시"
+                        defaultValue="08"
+                    />
+                    <NumericSelector
+                        start={0}
+                        end={50}
+                        step={10}
+                        targetLength={2}
+                        padChar="0"
+                        defaultValue="10"
+                        unit="분"
+                    />
                 </div>
             </form>
         </li>

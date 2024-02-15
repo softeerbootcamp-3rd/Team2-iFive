16ab4d3e987335790f44bf2232a325051c56f25c
육종호 <dbrwhdgh1203@naver.com>
2024-02-15 21:50:27 +0900

    chore: ParentMap 추가

--- FE/src/pages/Location/Location.jsx
+++ FE/src/pages/Location/Location.jsx
@@ -1,10 +1,11 @@
 import { useSearchParams } from "react-router-dom";
 import { DriverMap } from "../../components/Map/DriverMap";
+import ParentMap from "../../components/Map/ParentMap";
 
 export function Location() {
     const [query] = useSearchParams();
     const type = query.get("type");
-    const render = type == "driver" ? <DriverMap /> : <DriverMap />;
+    const render = type == "driver" ? <DriverMap /> : <ParentMap />;
 
     return <>{render}</>;
 }

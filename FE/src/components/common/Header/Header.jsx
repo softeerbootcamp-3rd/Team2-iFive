2463ed3f5c1d00bfc00140359ef91a309e59eb3d
Sang-minKIM <kimpran@naver.com>
2024-02-15 21:21:51 +0900

    feat: header 뒤로가기 추가

--- FE/src/components/common/Header/Header.jsx
+++ FE/src/components/common/Header/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styles from "./Header.module.scss";
 import backIcon from "@/assets/back.svg";
 
@@ -6,10 +7,12 @@ import backIcon from "@/assets/back.svg";
  * @param {string} title
  * @returns
  */
-export function Header({ title }) {
+export function Header({ title, back = -1 }) {
     return (
         <header className={styles.header}>
-            <img src={backIcon} />
+            <Link to={back}>
+                <img src={backIcon} />
+            </Link>
             <h2 className={styles.title}>{title}</h2>
         </header>
     );

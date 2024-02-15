61e1b42e34963a033db459603c80b3199d4726de
Sang-minKIM <kimpran@naver.com>
2024-02-15 18:32:32 +0900

    refactor: 요일 리스트 컴포넌트 추가

---
+++ FE/src/components/Search/DayList.jsx
@@ -0,0 +1,29 @@
+import styles from "../DayList.module.scss";
+import { LabelledList } from "../common/Layout/LabelledList";
+
+export function DayList() {
+    const handleWeekClick = (day) => {
+        setTimeList((prevTimeList) => {
+            return {
+                ...prevTimeList,
+                [day]: !prevTimeList[day]
+            };
+        });
+    };
+
+    const dayListElement = SEARCH_PAGE.WEEK.map((day) => (
+        <li
+            key={day}
+            className={`${styles.dayItem} ${timeList[day] && styles.active}`}
+            onClick={(event) => handleWeekClick(day, event)}
+        >
+            <p>{day[0]}</p>
+        </li>
+    ));
+
+    return (
+        <LabelledList articleStyle="dayBox" label="픽업 요일">
+            {dayListElement}
+        </LabelledList>
+    );
+}

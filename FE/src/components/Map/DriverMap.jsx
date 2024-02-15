f17cdc06ad374871b7b5926ba97b2082321002be
육종호 <dbrwhdgh1203@naver.com>
2024-02-15 15:40:52 +0900

    feat: 기사 픽업화면- 아이정보 get 요청 기능 추가

--- FE/src/components/Map/DriverMap.jsx
+++ FE/src/components/Map/DriverMap.jsx
@@ -1,15 +1,47 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./map.module.scss";
 import { useMap } from "../../hooks/useMap";
 import { useCoords } from "../../hooks/useCoords";
 import { getLatLng } from "../../utils/map";
 import { useMarker } from "../../hooks/useMarker";
 import { Loader } from "../common/Loader/Loader";
-import { WEBSOCKET_URL, ACCESS_TOKEN } from "../../constants/constants";
+import {
+    WEBSOCKET_URL,
+    ACCESS_TOKEN,
+    userType
+} from "../../constants/constants";
 import { getCurrentLocation } from "../../utils/coords";
 import { getAccessToken } from "../../utils/auth";
+import { getKidInfo } from "../../service/api";
+import { BottomSheet } from "../common/Bottomsheet/Bottomsheet";
+
+const initState = {
+    loading: "loading",
+    success: "success",
+    error: "error"
+};
+
+const childData = {
+    name: "육 아들",
+    time: "7:00~8:00",
+    start: "서울 시청",
+    goal: "국민대"
+};
 
 export function DriverMap() {
+    const [kidData, setKidData] = useState([]);
+    const [apiRequest, setApiRequest] = useState(initState.loading);
+
+    const getKidData = async () => {
+        try {
+            const kidData = await getKidInfo();
+            setApiRequest(initState.success);
+        } catch (error) {
+            setApiRequest(initState.error);
+            console.error(error);
+        }
+    };
+
     const mapElementRef = useRef();
     const {
         location: { latitude, longitude },
@@ -27,6 +59,10 @@ export function DriverMap() {
 
     const webSocketRef = useRef(null);
     const lastLocationRef = useRef({ latitude: null, longitude: null });
+
+    useEffect(() => {
+        getKidData();
+    }, []);
 
     useEffect(() => {
         if (!driverMarker) return;
@@ -93,6 +129,10 @@ export function DriverMap() {
         <div className={styles.wrapper}>
             {!map && <Loader />}
             <div ref={mapElementRef} id="map" className={styles.map} />
+            <BottomSheet
+                childData={childData}
+                userRole={userType.driver}
+            ></BottomSheet>
         </div>
     );
 }

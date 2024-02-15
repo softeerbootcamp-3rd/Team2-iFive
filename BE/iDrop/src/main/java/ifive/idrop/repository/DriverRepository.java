53182d4499e10dd4ec4b463b4a3cf78fa42df978
win9 <krrr883@gmail.com>
2024-02-15 18:06:12 +0900

    refactor: findByID Optional메소드 이용
    Optional메소드를 이용하여 코드를 한줄로 줄이도록 수정

--- BE/iDrop/src/main/java/ifive/idrop/repository/DriverRepository.java
+++ BE/iDrop/src/main/java/ifive/idrop/repository/DriverRepository.java
@@ -13,10 +13,6 @@ public class DriverRepository {
     private final EntityManager em;
 
     public Optional<Driver> findById(Long id) {
-        return em.createQuery("select d From Driver d where d.id =: id", Driver.class)
-                .setParameter("id", id)
-                .getResultList()
-                .stream()
-                .findAny();
+        return Optional.ofNullable(em.find(Driver.class, id));
     }
 }

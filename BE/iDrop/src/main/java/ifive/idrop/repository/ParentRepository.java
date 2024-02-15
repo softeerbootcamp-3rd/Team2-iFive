6bccc61b5d3f1b1c7def94f0bfef97b0496c2535
win9 <krrr883@gmail.com>
2024-02-15 17:47:00 +0900

    feat: 이동중인 child 조회 쿼리 구현
    pickUp테이블들의 join을 이용하여 시작 시간이 존재하면 이동중인 것으로 파악하여 null이 아니면 가져올 수 있도록 구현

--- BE/iDrop/src/main/java/ifive/idrop/repository/ParentRepository.java
+++ BE/iDrop/src/main/java/ifive/idrop/repository/ParentRepository.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -19,5 +20,16 @@ public class ParentRepository {
                 .getResultList()
                 .stream()
                 .findAny();
+    }
+
+    public List<Child> findRunningChild(Long parentId) {
+        String query = "SELECT c\n" +
+                "FROM Child c\n" +
+                "JOIN PickUpInfo pui ON c.id = pui.child.id\n" +
+                "JOIN PickUp pu ON pui.id = pu.pickupInfoId\n" +
+                "WHERE c.parentId = :parentId AND pu.reservedTime IS NOT NULL";
+        return em.createQuery(query, Child.class)
+                .setParameter("parentId", parentId)
+                .getResultList();
     }
 }

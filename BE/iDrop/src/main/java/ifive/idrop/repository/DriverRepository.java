e5f158703d74e10da7eb01364bf41fe57648bdcc
win9 <krrr883@gmail.com>
2024-02-16 16:31:01 +0900

    feat: 불필요한 조인 제거
    불필요하게 child 테이블을 조인하였던 쿼리 수정

--- BE/iDrop/src/main/java/ifive/idrop/repository/DriverRepository.java
+++ BE/iDrop/src/main/java/ifive/idrop/repository/DriverRepository.java
@@ -25,7 +25,6 @@ public class DriverRepository {
     public List<PickUpInfo> findRunningPickInfo(Long driverId) {
         String query = "SELECT pui\n" +
                 "FROM PickUpInfo pui\n" +
-                "JOIN Child c ON pui.id = c.id\n" +
                 "JOIN PickUp pu ON pui.id = pu.pickUpInfo.id\n" +
                 "WHERE pui.driver.id =: driverId AND pu.startTime IS NOT NULL";
         return em.createQuery(query, PickUpInfo.class)

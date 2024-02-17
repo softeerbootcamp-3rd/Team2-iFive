c1bb1a98f844a07e12f854c219867aebe9fdb398
육종호 <dbrwhdgh1203@naver.com>
2024-02-17 14:43:34 +0900

    refactor: 필요없는 코드 삭제 및 useFetch로 변경

--- FE/src/components/Map/ParentMap.jsx
+++ FE/src/components/Map/ParentMap.jsx
@@ -1,41 +1,17 @@
 import styles from "./map.module.scss";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { useCoords } from "../../hooks/useCoords";
 import { getLatLng } from "../../utils/map";
 import { useMap } from "../../hooks/useMap";
 import { Loader } from "../common/Loader/Loader";
 import { BottomSheet } from "../common/Bottomsheet/Bottomsheet";
 import { PARENT_TOKEN, WEBSOCKET_URL } from "../../constants/constants";
-import { getKidInfo } from "../../service/api";
 import { useMarker } from "../../hooks/useMarker";
-
-const childData = {
-    name: "육 아들",
-    time: "7:00~8:00",
-    start: "서울 시청",
-    goal: "국민대"
-};
-
-const state = {
-    loading: "loading",
-    success: "success",
-    error: "error"
-};
+import { useFetchGet } from "../../hooks/useFetch";
 
 export default function ParentMap() {
-    const [pickUpData, setPickUpdata] = useState();
-    const [apiState, setApiState] = useState(state.loading);
-
-    const getData = async () => {
-        try {
-            const response = await getKidInfo();
-            setApiState(state.success);
-            setPickUpdata(response);
-        } catch (error) {
-            setApiState(state.error);
-            console.error(error);
-        }
-    };
+    const [kidData, setKidData] = useFetchGet(query);
+    const { loading, error, data } = kidData;
 
     const mapElementRef = useRef();
     const {
@@ -80,7 +56,15 @@ export default function ParentMap() {
         <div className={styles.wrapper}>
             {!map && <Loader />}
             <div ref={mapElementRef} id="map" className={styles.map} />
-            <BottomSheet childData={childData}></BottomSheet>
+            {loading ? <Loader /> : <BottomSheet childData={childData} />}
         </div>
     );
 }
+
+const query = "/parent/pickup/now";
+const childData = {
+    name: "육 아들",
+    time: "7:00~8:00",
+    start: "서울 시청",
+    goal: "국민대"
+};

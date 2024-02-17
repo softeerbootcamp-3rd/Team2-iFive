d3cfa7ae3c2ba439ce9b5cfcdd4bc1c76fc1ac2e
육종호 <dbrwhdgh1203@naver.com>
2024-02-18 02:02:52 +0900

    refactor: 임시데이터 추가 및 기사, 부모 메뉴 각 컴포넌트화

--- FE/src/pages/Menu/Menu.jsx
+++ FE/src/pages/Menu/Menu.jsx
@@ -3,30 +3,22 @@ import { MenuButton } from "./menuButton";
 import iDropGreen from "@/assets/iDropGreen.svg";
 import { BottomSheet } from "../../components/common/Bottomsheet/Bottomsheet";
 import { MENU_PAGE } from "../../constants/constants";
+import Location from "@/assets/Location.svg";
+import Star from "@/assets/Star.svg";
+import Success from "@/assets/Success.svg";
+import User from "@/assets/user_icon.svg";
+import Calender from "@/assets/calender.svg";
+import Truck from "@/assets/truck.png";
 
-const userType = {
-    parent: 0,
-    driver: 1
-};
-const childData = {
-    name: "육 아들",
-    time: "7:00~8:00",
-    start: "서울 시청",
-    goal: "국민대"
-};
-
-// 현재 test를 위해 props의 default 값 부여. 추후 삭제 필요
-export function Menu({ userRole = 1, userName = "육종호" }) {
-    const { HELLO_MSG, INTRO_TEXT, MENU_LIST } = MENU_PAGE(userName, userRole);
-
-    //비동기 통신으로 userRole, userinfo 받아오기
-
+export function DriverMenu() {
+    const { HELLO_MSG, INTRO_TEXT, MENU_LIST } = DRIVER_MENU_PAGE("육종호");
     const renderMenuItems = () =>
         MENU_LIST.map(({ icon, text, route }, index) => (
             <MenuButton key={index} imgUrl={icon} text={text} route={route} />
         ));
 
     return (
+        // TODO: 컴포넌트 쪼개기
         <div className={styles.wrapper}>
             <img src={iDropGreen}></img>
             <div>
@@ -34,7 +26,65 @@ export function Menu({ userRole = 1, userName = "육종호" }) {
                 <span>{INTRO_TEXT}</span>
             </div>
             <div className={styles.menuContainer}>{renderMenuItems()}</div>
-            <BottomSheet childData={childData}></BottomSheet>
+            <BottomSheet childData={exampleData}></BottomSheet>
         </div>
     );
 }
+
+export function ParentMenu() {
+    const { HELLO_MSG, INTRO_TEXT, MENU_LIST } = PARENT_MENU_PAGE("육종호");
+    const renderMenuItems = () =>
+        MENU_LIST.map(({ icon, text, route }, index) => (
+            <MenuButton key={index} imgUrl={icon} text={text} route={route} />
+        ));
+
+    return (
+        // TODO: 컴포넌트 쪼개기
+        <div className={styles.wrapper}>
+            <img src={iDropGreen}></img>
+            <div>
+                <h1>{HELLO_MSG} </h1>
+                <span>{INTRO_TEXT}</span>
+            </div>
+            <div className={styles.menuContainer}>{renderMenuItems()}</div>
+            <BottomSheet childData={exampleData}></BottomSheet>
+        </div>
+    );
+}
+
+const exampleData = [
+    {
+        childName: "김하나",
+        childImage: "String...",
+        startAddress: "에티버스러닝 학동캠퍼스",
+        endAddress: "코마츠",
+        startDate: "2024.02.01",
+        endDate: "2024.02.29"
+    }
+];
+
+const DRIVER_MENU_PAGE = (userName) => ({
+    HELLO_MSG: `안녕하세요, ${userName}님`,
+    INTRO_TEXT: `오늘도 안전한 픽업 부탁드려요`,
+    MENU_LIST: [
+        { icon: Calender, text: "픽업 일정" },
+        { icon: Truck, text: "픽업 시작", route: "/pickup" },
+        { icon: User, text: "프로필" },
+        { icon: Success, text: "요청 목록" }
+    ]
+});
+
+const PARENT_MENU_PAGE = (userName) => ({
+    HELLO_MSG: `안녕하세요, ${userName}님`,
+    INTRO_TEXT: `오늘도 안전하게 픽업할게요`,
+    MENU_LIST: [
+        {
+            icon: Location,
+            text: "실시간 위치",
+            route: "/map?type=parent"
+        },
+        { icon: Success, text: "구독 하기" },
+        { icon: User, text: "우리 아이" },
+        { icon: Star, text: "이용 내역" }
+    ]
+});

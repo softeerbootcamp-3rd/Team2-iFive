a9814e922300e52daad1620e1544093a08abfbe0
eekrwl <wani9909@naver.com>
2024-02-18 20:20:11 +0900

    feat: 이미지 업로드 기능 구현
    upload 메서드에서 folderPath도 입력받아 재사용성을 높였다.

---
+++ BE/iDrop/src/main/java/ifive/idrop/service/ImageService.java
@@ -0,0 +1,37 @@
+package ifive.idrop.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Slf4j
+public class ImageService {
+
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String getImageUrl(String uploadString) {
+        return "" + amazonS3.getUrl(bucket, uploadString);
+    }
+
+    public String upload(MultipartFile multipartFile, String folderPath) throws IOException {
+        String s3FileName = folderPath + UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+        ObjectMetadata objMeta = new ObjectMetadata();
+        objMeta.setContentLength(multipartFile.getInputStream().available());
+        amazonS3.putObject(bucket, s3FileName, multipartFile.getInputStream(), objMeta);
+        return s3FileName;
+    }
+}

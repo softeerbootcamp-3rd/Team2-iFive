1411f7582acac789923388c3a2e7d19ad7632e2a
육종호 <dbrwhdgh1203@naver.com>
2024-02-18 15:04:17 +0900

    refactor: bottomsheet 내 들어갈 content 부모, 기사 분리

--- FE/src/components/common/Bottomsheet/KidInfoBox.jsx
+++ FE/src/components/common/Bottomsheet/KidInfoBox.jsx
@@ -1,3 +1,4 @@
+import { useLocation } from "react-router-dom";
 import styles from "./kidInfoBox.module.scss";
 import iDrop from "@/assets/iDropGreen.svg";
 
@@ -22,5 +23,64 @@ export function KidInfoComponent({
                 </span>
             </div>
         </div>
+    );
+}
+
+export function DriverContents({
+    childName,
+    childImage,
+    startAddress,
+    endAddress,
+    startTime,
+    endTime
+}) {
+    return (
+        <>
+            <h3>오늘의 픽업</h3>
+            <div className={styles.content}>
+                <img className={styles.kidImg} src={childImage || iDrop}></img>
+                <div className={styles.infoBox}>
+                    <span>{childName}</span>
+                    <span>
+                        {startTime || "2024.02.01"} ~ {endTime || "2024.02.28"}
+                    </span>
+                    <span>
+                        {startAddress} {"→"} {endAddress}
+                    </span>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export function ParentContents({
+    childName,
+    childImage,
+    startAddress,
+    endAddress,
+    startDate,
+    endDate,
+    startTime,
+    endTime
+}) {
+    const { pathname } = useLocation();
+    const MSG =
+        pathname === "/map"
+            ? `${startTime} ~ ${endTime}`
+            : `${startDate} ~ ${endDate}`;
+    return (
+        <>
+            <h3>픽업 쏴비수 구독 중</h3>
+            <div className={styles.content}>
+                <img className={styles.kidImg} src={childImage || iDrop}></img>
+                <div className={styles.infoBox}>
+                    <span>{childName}</span>
+                    <span>{MSG}</span>
+                    <span>
+                        {startAddress} {"→"} {endAddress}
+                    </span>
+                </div>
+            </div>
+        </>
     );
 }

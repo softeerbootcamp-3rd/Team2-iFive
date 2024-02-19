d8323ae4b4eec7af86293b3e0ea10bb2801e20a8
육종호 <dbrwhdgh1203@naver.com>
2024-02-19 23:57:22 +0900

    feat: 사진 촬영 기능 추가 및 서버 전송 기능 구현

--- FE/src/pages/PickUp/PickUp.jsx
+++ FE/src/pages/PickUp/PickUp.jsx
@@ -1,53 +1,111 @@
-import { useEffect, useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { Footer } from "../../components/common/Footer/Footer";
-import { KidInfoComponent } from "../../components/common/Bottomsheet/KidInfoBox";
+import { DriverContents } from "../../components/common/Bottomsheet/KidInfoBox";
 import { Header } from "../../components/common/Header/Header";
 import styles from "./PickUp.module.scss";
+import { CameraCapture } from "./cameraCapture";
+import { postKidInfo } from "../../service/api";
+import { isWithinRadius } from "../../utils/calculatorDistance";
+import { useCoords } from "../../hooks/useCoords";
 
-const exampleData = {
-    childName: "김하나",
-    childImage: "String...",
-    startAddress: "에티버스러닝 학동캠퍼스",
-    endAddress: "코마츠",
-    startDate: "9:00",
-    endDate: "10:00"
-};
+export default function PickUpPage() {
+    const [validLocation, setCrntLocation] = useState(false);
 
-export function PickUpPage({ childData }) {
-    // TO DO: 지금 픽업할 아이 정보 받아오기
+    const kidData = getKidData();
+    console.log(kidData);
+
+    const {
+        location: { latitude, longitude },
+        isLoading
+    } = useCoords();
+
+    const notes = useRef();
+
+    const [image, setImage] = useState();
+
+    const onSetImage = (image) => {
+        setImage(image);
+        let checkLocation;
+        if (!flag) {
+            checkLocation = isWithinRadius(
+                kidData.startLatitude,
+                kidData.startLongitude,
+                37.5138649,
+                127.0295296
+            );
+        } else {
+            checkLocation = isWithinRadius(
+                kidData.endLatitude,
+                kidData.endLongitude,
+                latitude,
+                longitude
+            );
+        }
+        setCrntLocation(checkLocation);
+    };
+    console.log(validLocation);
 
     const { state } = useLocation();
     const flag = state?.flag;
 
     const navigate = useNavigate();
     const movePage = (route) => {
-        navigate(`${route}`);
+        navigate(`${route}`, { state: { kidData: kidData } });
     };
 
-    const handleClick = () => {
+    const handleClick = async () => {
+        if (!image) {
+            alert("이미지 업로드 해주세요!");
+            return;
+        }
+        const formData = new FormData();
+        formData.append("childId", kidData.childId);
+        formData.append("image", image);
+        formData.append("message", notes.current.value);
+        await postKidInfo(formData);
+
         flag ? movePage("/publish") : movePage("/map?type=driver");
     };
+
     return (
         <div className={styles.wrapper}>
             <Header title={flag ? "픽업 종료" : "픽업 시작"}></Header>
             <div className={styles.contents}>
-                <KidInfoComponent {...exampleData} />
-                <div className={styles.imgContainer}>사진 등록</div>
+                <DriverContents {...kidData} />
+                <CameraCapture onSetImage={onSetImage} />
                 <div className={styles.textarea}>
                     <label htmlFor="significant">특이사항</label>
                     <textarea
+                        ref={notes}
                         id="significant"
                         name="significant"
-                        rows={10}
+                        rows={5}
                         placeholder="오늘 픽업 시 특의사항이 있다면 작성해주세요"
                     ></textarea>
                 </div>
             </div>
-            <Footer
+            {/* <Footer
                 text={flag ? "픽업 종료" : "픽업 시작"}
                 onClick={handleClick}
-            ></Footer>
+            ></Footer> */}
+            {validLocation ? (
+                <Footer
+                    text={flag ? "픽업 종료" : "픽업 시작"}
+                    onClick={handleClick}
+                ></Footer>
+            ) : (
+                <Footer
+                    isButtonDisabled="true"
+                    text={flag ? "픽업 종료" : "픽업 시작"}
+                    onClick={handleClick}
+                ></Footer>
+            )}
         </div>
     );
+}
+
+function getKidData() {
+    const location = useLocation();
+    return location.state.kidData;
 }

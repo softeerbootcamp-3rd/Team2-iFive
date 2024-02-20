5cc2bcc6c446e3933e397a55a79375a1ac658ca3
eekrwl <wani9909@naver.com>
2024-02-20 11:34:56 +0900

    chore: /pickup -> /driver/pickup
    parent가 POST /pickup 요청 시 500에러 나는 부분 수정
    코드를 보니 알아서 걸려주려면 /driver를 달아주면 된다.

--- BE/iDrop/src/main/java/ifive/idrop/controller/PickUpController.java
+++ BE/iDrop/src/main/java/ifive/idrop/controller/PickUpController.java
@@ -24,7 +24,7 @@ public class PickUpController {
 
     private final PickUpService pickUpService;
 
-    @PostMapping("/pickup")
+    @PostMapping("/driver/pickup")
     public BaseResponse<String> startPickUp(@Login Driver driver, Long childId, @ModelAttribute MultipartFile image, String message) {
         PickUp pickUp = pickUpService.findCurrentPickUp(driver.getId(), childId)
                 .orElseThrow(() -> new CommonException(ErrorCode.CURRENT_PICKUP_NOT_FOUND));

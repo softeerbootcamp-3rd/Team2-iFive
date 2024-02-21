80fb1f464749d30a899da60dded7c122b4d9500c
Sang-minKIM <kimpran@naver.com>
2024-02-21 17:14:50 +0900

    feat: 이용내역 비동기 요청 추가

--- FE/src/pages/History/History.jsx
+++ FE/src/pages/History/History.jsx
@@ -2,9 +2,13 @@ import { Header } from "../../components/common/Header/Header";
 import styles from "./History.module.scss";
 import { fakeHistoryData } from "../../service/mockdata";
 import { HistoryItem } from "./HistoryItem/HistoryItem";
+import { getSubscriptionHistoryList } from "./api";
+import { useLocation } from "react-router-dom";
 
 export default function History() {
-    const contentElement = fakeHistoryData.map((historyData) => (
+    const historyListData = useLocation();
+
+    const contentElement = historyListData.map((historyData) => (
         <HistoryItem key={historyData.pickUpInfoId} historyData={historyData} />
     ));
 
@@ -14,4 +18,9 @@ export default function History() {
             <main className={styles.content}>{contentElement}</main>
         </div>
     );
+}
+
+export async function loader() {
+    const response = await getSubscriptionHistoryList();
+    return response;
 }

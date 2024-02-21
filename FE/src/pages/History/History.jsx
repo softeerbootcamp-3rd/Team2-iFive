907f8e7d7b9b98bef808ac44f85e0994c278dea3
Sang-minKIM <kimpran@naver.com>
2024-02-21 16:44:21 +0900

    style: 이용내역 목록 퍼블리싱

--- FE/src/pages/History/History.jsx
+++ FE/src/pages/History/History.jsx
@@ -1,7 +1,7 @@
 import { Header } from "../../components/common/Header/Header";
 import styles from "./History.module.scss";
-import downArrowIcon from "@/assets/downArrow.svg";
 import { fakeHistoryData } from "../../service/mockdata";
+import { HistoryItem } from "./components/HistoryItem/HistoryItem";
 
 export default function History() {
     const contentElement = fakeHistoryData.map((historyData) => (
@@ -14,89 +14,4 @@ export default function History() {
             <main className={styles.content}>{contentElement}</main>
         </div>
     );
-}
-
-function HistoryItem({ historyData }) {
-    const {
-        driverName,
-        driverImage,
-        startDate,
-        endDate,
-        startAddress,
-        endAddress,
-        status,
-        schedule
-    } = formatHistoryData(historyData);
-
-    const scheduleListElement = schedule.map((scheduleData) => (
-        <ScheduleItem key={scheduleData.day} scheduleData={scheduleData} />
-    ));
-
-    return (
-        <section className={styles.historyItem}>
-            <div className={styles.driverInfo}>
-                <div className={styles.driverInfoText}>
-                    <div className={styles.driverNameWrapper}>
-                        <span className={styles.driverName}>{driverName}</span>
-                        <div className={styles.pickupStatus}>{status}</div>
-                    </div>
-                    <span
-                        className={styles.period}
-                    >{`${startDate} ~ ${endDate}`}</span>
-                    <span className={styles.departure}>{startAddress}</span>
-                    <span className={styles.destination}>{endAddress}</span>
-                </div>
-                <img
-                    className={styles.driverImg}
-                    src={driverImage}
-                    alt="기사님 사진"
-                />
-            </div>
-            <ul className={styles.scheduleList}>{scheduleListElement}</ul>
-            <img src={downArrowIcon} alt="아래 화살표" />
-        </section>
-    );
-}
-
-function ScheduleItem({ scheduleData }) {
-    return (
-        <li
-            className={styles.scheduleItem}
-        >{`${scheduleData.day} ${scheduleData.hour}:${scheduleData.min}`}</li>
-    );
-}
-
-function transformSchedule(schedule) {
-    const daysInfo = {
-        Mon: { order: 1, translation: "월" },
-        Tue: { order: 2, translation: "화" },
-        Wed: { order: 3, translation: "수" },
-        Thu: { order: 4, translation: "목" },
-        Fri: { order: 5, translation: "금" },
-        Sat: { order: 6, translation: "토" },
-        Sun: { order: 7, translation: "일" }
-    };
-
-    const makeScheduleObject = (day) => ({
-        day,
-        min: schedule[day].min,
-        hour: schedule[day].hour
-    });
-
-    const translateDay = (schedule) => ({
-        ...schedule,
-        day: daysInfo[schedule.day].translation
-    });
-
-    const sortByDay = (a, b) => daysInfo[a.day].order - daysInfo[b.day].order;
-
-    const scheduleArray = Object.keys(schedule).map(makeScheduleObject);
-    scheduleArray.sort(sortByDay);
-    const translatedScheduleArray = scheduleArray.map(translateDay);
-
-    return translatedScheduleArray;
-}
-
-function formatHistoryData(data) {
-    return { ...data, schedule: transformSchedule(data.schedule) };
 }

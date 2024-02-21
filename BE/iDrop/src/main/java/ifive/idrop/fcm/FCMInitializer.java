8e4eb520ebd4ac14cf33a96bb6d3108192a2a9ff
win9 <krrr883@gmail.com>
2024-02-21 10:15:42 +0900

    feat: FCM 초기세팅

---
+++ BE/iDrop/src/main/java/ifive/idrop/fcm/FCMInitializer.java
@@ -0,0 +1,34 @@
+package ifive.idrop.fcm;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class FCMInitializer {
+    @Value("${fcm.path}")
+    private String FIREBASE_CONFIG_PATH;
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            GoogleCredentials googleCredentials = GoogleCredentials
+                    .fromStream(new ClassPathResource(FIREBASE_CONFIG_PATH).getInputStream());
+            log.info("FIREBASE_CONFIG_PATH_SETTING = {}", FIREBASE_CONFIG_PATH);
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(googleCredentials)
+                    .build();
+            FirebaseApp.initializeApp(options);
+        } catch (IOException e) {
+            log.error("FCM error message = {}", e.getMessage());
+        }
+    }
+}

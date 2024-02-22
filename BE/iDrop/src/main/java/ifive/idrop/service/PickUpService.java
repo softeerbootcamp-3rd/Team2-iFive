b84692ecfa98fd469053f5ebb92a503f369a0eb0
win9 <krrr883@gmail.com>
2024-02-22 19:03:17 +0900

    feat: 기사 픽업 알림 구현
    기사의 픽업이 시작과 종료가 되면 부모에게 알림이 갈 수 있도록 구현

--- BE/iDrop/src/main/java/ifive/idrop/service/PickUpService.java
+++ BE/iDrop/src/main/java/ifive/idrop/service/PickUpService.java
@@ -1,8 +1,11 @@
 package ifive.idrop.service;
 
+import ifive.idrop.entity.Parent;
 import ifive.idrop.entity.PickUp;
 import ifive.idrop.exception.CommonException;
 import ifive.idrop.exception.ErrorCode;
+import ifive.idrop.fcm.AlarmMessage;
+import ifive.idrop.fcm.NotificationUtill;
 import ifive.idrop.repository.PickUpRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,9 +14,9 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 
 @Slf4j
 @Service
@@ -25,17 +28,24 @@ public class PickUpService {
     private final String PICKUP_IMAGE_PATH = "image/pickup/";
 
     @Transactional
-    public void saveStartOrEndPickUp(Long pickUpId, MultipartFile image, String message) throws IOException {
+    public void saveStartOrEndPickUp(Long pickUpId, MultipartFile image, String message) throws IOException, ExecutionException, InterruptedException {
         PickUp pickUp = pickUpRepository.findPickUpById(pickUpId)
                 .orElseThrow(() -> new CommonException(ErrorCode.PICKUP_NOT_FOUND));
+        Parent parent = pickUp.getPickUpInfo().getChild().getParent();
         if (pickUp.getStartImage() == null) {
             String imageUrl = imageService.upload(image, PICKUP_IMAGE_PATH);
             pickUpRepository.savePickUpStartInfo(pickUpId, imageUrl, message);
             log.info("pickUp Start - driverId = {}, pickUpId = {}", pickUp.getDriver().getId(), pickUp.getId());
+
+            NotificationUtill.createNotification(parent, AlarmMessage.PICK_UP_START.getTitle(),
+                    AlarmMessage.PICK_UP_END.getMessage());
         } else if (pickUp.getEndImage() == null) {
             String imageUrl = imageService.upload(image, PICKUP_IMAGE_PATH);
             pickUpRepository.savePickUpEndInfo(pickUpId, imageUrl, message);
             log.info("pickUp End - driverId = {}, pickUpId = {}", pickUp.getDriver().getId(), pickUp.getId());
+
+            NotificationUtill.createNotification(parent, AlarmMessage.PICK_UP_START.getTitle(),
+                    AlarmMessage.PICK_UP_END.getMessage());
         } else {
             throw new CommonException(ErrorCode.PICKUP_ALREADY_END);
         }

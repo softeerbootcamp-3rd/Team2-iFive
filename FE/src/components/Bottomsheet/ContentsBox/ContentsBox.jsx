c438717293f4db7a057af0b26f37c07e3d625e52
육종호 <dbrwhdgh1203@naver.com>
2024-02-23 02:03:54 +0900

    refactor: 컴포넌트 간소화 및 의존성 제거

--- FE/src/components/Bottomsheet/ContentsBox/ContentsBox.jsx
+++ FE/src/components/Bottomsheet/ContentsBox/ContentsBox.jsx
@@ -2,65 +2,36 @@ import { useLocation, useNavigate } from "react-router-dom";
 import styles from "../KidInfoBox/kidInfoBox.module.scss";
 import iDrop from "@/assets/iDropGreen.svg";
 
-export function ContentsBox({ childrenData, type }) {
+export function ContentsBox({
+    childId,
+    childImage,
+    childName,
+    pickUpStartTime,
+    pickUpEndTime,
+    startAddress,
+    endAddress,
+    startDate,
+    endDate,
+    type
+}) {
     const { pathname } = useLocation();
-    const isSelectPage = pathname === "/select" ? true : false;
-    const navigator = useNavigate();
-    const movePage = (index) => {
-        navigator("/pickup", { state: childrenData[index] });
-    };
 
-    let childrenList = [];
-    if (Array.isArray(childrenData)) {
-        childrenList = [...childrenData]; // 배열인 경우 복사하여 할당
-    } else {
-        childrenList = [childrenData]; // 배열이 아닌 경우 배열에 담아 할당
-    }
-
-    const render = childrenList.map(
-        (
-            {
-                childId,
-                childImage,
-                childName,
-                pickUpStartTime,
-                pickUpEndTime,
-                startAddress,
-                endAddress,
-                startDate,
-                endDate
-            },
-            index
-        ) => {
-            let timeMsg =
-                type === "parent"
-                    ? pathname === "/map"
-                        ? `${pickUpStartTime} ~ ${pickUpEndTime}`
-                        : `${startDate} ~ ${endDate}`
-                    : `${pickUpStartTime} ~ ${pickUpEndTime}`;
-            return (
-                <div key={index} className={styles.content}>
-                    <img
-                        className={styles.kidImg}
-                        src={childImage || iDrop}
-                    ></img>
-                    <div className={styles.infoBox}>
-                        <span> {childName} </span>
-                        {isSelectPage ? (
-                            <button onClick={() => movePage(index)}>
-                                픽업시작
-                            </button>
-                        ) : (
-                            ""
-                        )}
-                        <span>{timeMsg}</span>
-                        <span>
-                            {startAddress} {"→"} {endAddress}
-                        </span>
-                    </div>
-                </div>
-            );
-        }
+    let timeMsg =
+        type === "parent"
+            ? pathname === "/map"
+                ? `${pickUpStartTime} ~ ${pickUpEndTime}`
+                : `${startDate} ~ ${endDate}`
+            : `${pickUpStartTime} ~ ${pickUpEndTime}`;
+    return (
+        <div className={styles.content}>
+            <img className={styles.kidImg} src={childImage || iDrop}></img>
+            <div className={styles.infoBox}>
+                <span> {childName} </span>
+                <span>{timeMsg}</span>
+                <span>
+                    {startAddress} <br /> {"→"} {endAddress}
+                </span>
+            </div>
+        </div>
     );
-    return render;
 }

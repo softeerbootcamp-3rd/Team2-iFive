f096670da5752bff106324620149be0c4bf935b8
육종호 <dbrwhdgh1203@naver.com>
2024-02-22 22:38:46 +0900

    chore: 필요없는 코드 삭제

--- FE/src/components/Bottomsheet/ContentsBox/ContentsBox.jsx
+++ FE/src/components/Bottomsheet/ContentsBox/ContentsBox.jsx
@@ -49,8 +49,7 @@ export function ContentsBox({ childrenData, type }) {
                             <span> {childName} </span>
                             {isSelectPage ? (
                                 <button onClick={() => movePage(index)}>
-                                    {" "}
-                                    픽업시작{" "}
+                                    픽업시작
                                 </button>
                             ) : (
                                 ""

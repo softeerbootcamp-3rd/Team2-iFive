c0e13660adb1633882f1b47f221a38beb18aea0e
육종호 <dbrwhdgh1203@naver.com>
2024-02-22 23:49:35 +0900

    feat: fetchPickUpList 함수 구현

--- FE/src/pages/Driver/PickUp/SelectChild.jsx
+++ FE/src/pages/Driver/PickUp/SelectChild.jsx
@@ -1,10 +1,13 @@
+import { useLoaderData, useLocation, useNavigate } from "react-router-dom";
 import { Header } from "@/components/Header/Header";
 import { ContentsBox } from "@/components/Bottomsheet/ContentsBox/ContentsBox";
+import { getKidInfo } from "@/service/childrenAPI";
 import styles from "./SelectChild.module.scss";
-import { useLocation, useNavigate } from "react-router-dom";
 
 export default function SelectChild() {
     const { state: childrenData } = useLocation();
+    //TODO: 페이지 접근 시 남아있는 픽업 리스트 요청. 없는 경우 접근을 제한
+    // const childrenData = useLoaderData();
     return (
         <>
             <Header title="픽업 선택" />
@@ -13,4 +16,9 @@ export default function SelectChild() {
             </div>
         </>
     );
+}
+
+export async function fetchPickUpList() {
+    const pickUpList = await getKidInfo();
+    return pickUpList.data;
 }

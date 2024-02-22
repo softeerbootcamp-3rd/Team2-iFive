dcd6d01bbc2d52a1b61c8d1f8a0b706be1b5933a
Sang-minKIM <kimpran@naver.com>
2024-02-23 00:08:29 +0900

    chore: jsDoc 추가

--- FE/src/components/Footer/Footer.jsx
+++ FE/src/components/Footer/Footer.jsx
@@ -1,9 +1,12 @@
+import React from "react";
 import styles from "./Footer.module.scss";
 
 /**
- *
- * @param {string} text
- * @returns
+ * @param {Object} props
+ * @param {string} props.text
+ * @param {()=> void} props.onClick
+ * @param {boolean} props.isButtonDisabled
+ * @returns {React.ReactNode}
  */
 export function Footer({ text, onClick, isButtonDisabled = false }) {
     return (

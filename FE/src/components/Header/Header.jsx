841aa227f792d6f2e066e54a484325d24dfc39e7
Sang-minKIM <kimpran@naver.com>
2024-02-23 00:41:37 +0900

    chore: remove import

--- FE/src/components/Header/Header.jsx
+++ FE/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import styles from "./Header.module.scss";
 import backIcon from "@/assets/back.svg";
 

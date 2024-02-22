65844af8af8cbc602d8034a4c745e410d30a8802
육종호 <dbrwhdgh1203@naver.com>
2024-02-22 18:57:49 +0900

    refactor: childrenData type에 따른 처리 구현

--- FE/src/components/Bottomsheet/ContentsBox/ContentsBox.jsx
+++ FE/src/components/Bottomsheet/ContentsBox/ContentsBox.jsx
@@ -1,21 +1,37 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import styles from "../KidInfoBox/kidInfoBox.module.scss";
 import iDrop from "@/assets/iDropGreen.svg";
 
 export function ContentsBox({ childrenData, type }) {
     const { pathname } = useLocation();
+    const isSelectPage = pathname === "/select" ? true : false;
+    const navigator = useNavigate();
+    const movePage = (index) => {
+        navigator("/pickup", { state: childrenData[index] });
+    };
 
-    const render = childrenData.map(
-        ({
-            childId,
-            childImage,
-            childName,
-            pickUpStartTime,
-            pickUpEndTime,
-            startAddress,
-            endAddress,
-            startDate,
-            endDate
-        }) => {
+    let childrenList = [];
+    if (Array.isArray(childrenData)) {
+        childrenList = [...childrenData]; // 배열인 경우 복사하여 할당
+    } else {
+        childrenList = [childrenData]; // 배열이 아닌 경우 배열에 담아 할당
+    }
+
+    const render = childrenList.map(
+        (
+            {
+                childId,
+                childImage,
+                childName,
+                pickUpStartTime,
+                pickUpEndTime,
+                startAddress,
+                endAddress,
+                startDate,
+                endDate
+            },
+            index
+        ) => {
             let timeMsg =
                 type === "parent"
                     ? pathname === "/map"
@@ -23,13 +39,23 @@ export function ContentsBox({ childrenData, type }) {
                         : `${startDate} ~ ${endDate}`
                     : `${pickUpStartTime} ~ ${pickUpEndTime}`;
             return (
-                <div key={childId} className={styles.content}>
+                <div key={index} className={styles.content}>
                     <img
                         className={styles.kidImg}
                         src={childImage || iDrop}
                     ></img>
                     <div className={styles.infoBox}>
-                        <span>{childName}</span>
+                        <div>
+                            <span> {childName} </span>
+                            {isSelectPage ? (
+                                <button onClick={() => movePage(index)}>
+                                    {" "}
+                                    픽업시작{" "}
+                                </button>
+                            ) : (
+                                ""
+                            )}
+                        </div>
                         <span>{timeMsg}</span>
                         <span>
                             {startAddress} {"→"} {endAddress}

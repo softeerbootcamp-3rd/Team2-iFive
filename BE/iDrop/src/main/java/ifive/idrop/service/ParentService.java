5e6b3e5483a11cc133ebb07f7b1f83f28e446ce3
win9 <krrr883@gmail.com>
2024-02-22 19:01:40 +0900

    feat: 부모 구독요청 알림 구현
    부모가 구독을 요청하면 기사에게 알림이 갈 수 있도록 구현

--- BE/iDrop/src/main/java/ifive/idrop/service/ParentService.java
+++ BE/iDrop/src/main/java/ifive/idrop/service/ParentService.java
@@ -11,6 +11,8 @@ import ifive.idrop.entity.*;
 import ifive.idrop.entity.enums.PickUpStatus;
 import ifive.idrop.exception.CommonException;
 import ifive.idrop.exception.ErrorCode;
+import ifive.idrop.fcm.AlarmMessage;
+import ifive.idrop.fcm.NotificationUtill;
 import ifive.idrop.repository.DriverRepository;
 import ifive.idrop.repository.ParentRepository;
 import ifive.idrop.repository.PickUpRepository;
@@ -25,6 +27,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import static ifive.idrop.util.ScheduleUtils.calculateEndDate;
 import static ifive.idrop.util.ScheduleUtils.calculateStartDate;
@@ -36,8 +39,7 @@ public class ParentService {
     private final ParentRepository parentRepository;
     private final PickUpRepository pickUpRepository;
 
-    @Transactional
-    public BaseResponse<String> createSubscribe(Parent parent, SubscribeRequest subscribeRequest) throws JSONException {
+    public BaseResponse<String> createSubscribe(Parent parent, SubscribeRequest subscribeRequest) throws JSONException, ExecutionException, InterruptedException {
         Driver driver = driverRepository.findById(subscribeRequest.getDriverId())
                 .orElseThrow(() -> new CommonException(ErrorCode.DRIVER_NOT_EXIST));
         Child child = parentRepository.findChild(parent.getId())
@@ -45,11 +47,11 @@ public class ParentService {
 
         PickUpSubscribe subscribe = createPickUpSubscribe();
         PickUpLocation location = createPickUpLocation(subscribeRequest);
-        PickUpInfo pickUpInfo = createPickUpInfo(subscribeRequest, child, driver, location, subscribe);
-
-        //TODO Alarm to Driver
         createPickUpInfo(subscribeRequest, child, driver, location, subscribe);
 
+        // 요청한 기사에게 알람
+        NotificationUtill.createNotification(driver, AlarmMessage.SUBSCRIBE_REQUEST.getTitle(),
+                AlarmMessage.SUBSCRIBE_REQUEST.getMessage());
         return BaseResponse.success();
     }
 
@@ -73,7 +75,7 @@ public class ParentService {
     // todo: 데모 이후 구독 생성시 후에 모두 승인이 아닌 대기 상태로 변경
     private PickUpSubscribe createPickUpSubscribe() {
         PickUpSubscribe subscribe = PickUpSubscribe.builder()
-                .status(PickUpStatus.ACCEPT)
+                .status(PickUpStatus.WAIT)
                 .requestDate(LocalDateTime.now())
                 .expiredDate(LocalDateTime.now().plusDays(1).plusWeeks(4))
                 .build();

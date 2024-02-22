b3611199e1b3e0f8d91f99b14acec4ac387578c9
eekrwl <wani9909@naver.com>
2024-02-22 15:19:43 +0900

    refactor: 네이버 경로를 가져오는 기능만 수행하도록 변경
    longitude,latitude와 같은 모양은 이 클래스에서만 사용
    Double형으로 시작,끝 경도,위도를 받아서 경로 리턴
    네이버 경로를 가져오는 역할만 수행

--- BE/iDrop/src/main/java/ifive/idrop/websocket/direction/NaverDirectionFinder.java
+++ BE/iDrop/src/main/java/ifive/idrop/websocket/direction/NaverDirectionFinder.java
@@ -1,6 +1,5 @@
 package ifive.idrop.websocket.direction;
 
-import ifive.idrop.entity.PickUpLocation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -25,31 +24,31 @@ public class NaverDirectionFinder {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
-    public Direction getDirection(String start, String goal) throws Exception {
-        StringBuilder sb = new StringBuilder(NAVER_DIRECTION_URL);
-        sb.append("?start=").append(start).append("&goal=").append(goal);
-        String url = sb.toString();
+    public Direction getDirection(Double startLongitude, Double startLatitude, Double endLongitude, Double endLatitude) throws Exception {
+        String url = makeUrl(startLongitude, startLatitude, endLongitude, endLatitude);
+        HttpHeaders headers = makeHeader();
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("X-NCP-APIGW-API-KEY-ID", clientId);
-        headers.set("X-NCP-APIGW-API-KEY", clientSecret);
-
-        RequestEntity<Void> req = RequestEntity
+        RequestEntity<Void> request = RequestEntity
                 .get(url)
                 .headers(headers)
                 .build();
 
-        NaverDirectionResponse response = restTemplate.exchange(req, NaverDirectionResponse.class).getBody();
-        Direction direction = new Direction(response.getPath());
-        return direction;
+        NaverDirectionResponse response = restTemplate.exchange(request, NaverDirectionResponse.class).getBody();
+        return new Direction(response.getPath());
     }
 
-    public String getStartLocationForApi(PickUpLocation pickUpLocation) {
-        return pickUpLocation.getStartLongitude() + "," + pickUpLocation.getStartLatitude();
+    private String makeUrl(Double startLongitude, Double startLatitude, Double endLongitude, Double endLatitude) {
+        StringBuilder sb = new StringBuilder(NAVER_DIRECTION_URL);
+        sb.append("?start=").append(startLongitude).append(",").append(startLatitude);
+        sb.append("&goal=").append(endLongitude).append(",").append(endLatitude);
+        return sb.toString();
     }
 
-    public String getEndLocationForApi(PickUpLocation pickUpLocation) {
-        return pickUpLocation.getEndLongitude() + "," + pickUpLocation.getEndLatitude();
+    private HttpHeaders makeHeader() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("X-NCP-APIGW-API-KEY-ID", clientId);
+        headers.set("X-NCP-APIGW-API-KEY", clientSecret);
+        return headers;
     }
 }

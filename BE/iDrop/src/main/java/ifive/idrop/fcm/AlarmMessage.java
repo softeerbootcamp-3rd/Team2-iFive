8f94a0ab9d0de1c382a0799afbe10bcf578855e0
win9 <krrr883@gmail.com>
2024-02-23 10:52:17 +0900

    chore: 알림 내용 변경
    픽업 예정 알림에 대한 정확한 정보를 보여주기 위한 내용 변경

--- BE/iDrop/src/main/java/ifive/idrop/fcm/AlarmMessage.java
+++ BE/iDrop/src/main/java/ifive/idrop/fcm/AlarmMessage.java
@@ -9,7 +9,7 @@ public enum AlarmMessage {
     PICK_UP_START("픽업 시작 안내", "기사가 픽업을 시작하였습니다"),
     PICK_UP_END("픽업 종료 안내", "기사가 픽업을 종료하였습니다"),
     SUBSCRIBE_REQUEST("구독 요청 안내", "구독 승인 요청이 왔습니다."),
-    PICK_UP_PRE_INFO("픽업 알림 안내", "픽업 예정까지 한 시간 남았습니다.")
+    PICK_UP_PRE_INFO("픽업 사전 안내", "픽업 시작까지 한 시간 남았습니다.")
     ;
 
     private final String title;

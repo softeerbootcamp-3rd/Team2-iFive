559689a0a19d105c9c90d0b1fc405524fd7ae2e1
yook-jongho <dbrwhdgh1203@naver.com>
2024-03-08 18:31:06 +0900

    refactor: 컴포넌트 분리

--- FE/src/pages/Driver/SubscriptionManagement/SubscriptionManagement.jsx
+++ FE/src/pages/Driver/SubscriptionManagement/SubscriptionManagement.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import styles from "./SubscriptionManagement.module.scss";
-import iDrop from "@/assets/iDropGreen.svg";
-import { transformSchedule } from "../../Parents/History/HistoryItem/transformSchedule";
-import { isHaveItems, removeCityPrefix } from "../../../utils/parseData";
+import { isHaveItems } from "../../../utils/parseData";
 import { Header } from "@/components/Header/Header";
 import { getKidInfo } from "@/service/childrenAPI";
 import { redirect } from "react-router-dom";
-import { ScheduleList } from "../../../components/Schedule/ScheduleList";
-import { postSubscribeRequest } from "../../../service/driverAPI";
+import { KidInformationBox } from "./KidInformationBox";
 import { useFetch } from "../../../hooks/useFetch";
 import { NoChildItems } from "../../../components/Layout/Content/EmptyChildData";
 import { Loader } from "../../../components/Loader/Loader";
@@ -43,105 +39,7 @@ export default function SubscriptionManagement() {
     );
 }
 
-function KidInformationBox({
-    subscripData: {
-        pickUpInfoId,
-        childImage,
-        childName,
-        childBirth,
-        childGender,
-        startDate,
-        endDate,
-        startAddress,
-        endAddress,
-        parentName,
-        parentPhoneNumber,
-        status,
-        schedule
-    },
-    fetchData
-}) {
-    startAddress = removeCityPrefix(startAddress);
-    endAddress = removeCityPrefix(endAddress);
-    schedule = transformSchedule(schedule);
 
-    const handleSubscription = async (pickUpId, status) => {
-        const postData = {
-            pickUpInfoId: pickUpId,
-            statusCode: status
-        };
-        await postSubscribeRequest(postData);
-        fetchData();
-    };
-
-    return (
-        <div className={styles.content}>
-            <div className={styles.kidInfo}>
-                <img className={styles.kidImg} src={childImage || iDrop}></img>
-                <div className={styles.infoBox}>
-                    <div className={styles.profiles}>
-                        <span className={styles.name}>{childName}</span>
-                        <div
-                            className={`${styles.status} ${status === "대기" ? styles.waiting : styles.accept}`}
-                        >
-                            {status}
-                        </div>
-                    </div>
-                    <span className={styles.birthDay}>
-                        {childGender}, {childBirth}
-                    </span>
-
-                    <span>
-                        {startDate} ~ {endDate}
-                    </span>
-                    <span>
-                        {parentName} / {parentPhoneNumber}
-                    </span>
-                </div>
-            </div>
-            <div>
-                <span>
-                    {startAddress} <br /> {"→"} {endAddress}
-                </span>
-            </div>
-            <ScheduleList schedule={schedule} status={status} />
-            <div className={styles.btnBox}>
-                {status !== "승인" && (
-                    <>
-                        <button
-                            className={styles.denyButton}
-                            onClick={() =>
-                                handleSubscription(
-                                    pickUpInfoId,
-                                    statusCode.refuse
-                                )
-                            }
-                        >
-                            거절
-                        </button>
-                        <button
-                            className={styles.acceptButton}
-                            onClick={async () => {
-                                await handleSubscription(
-                                    pickUpInfoId,
-                                    statusCode.accept
-                                );
-                                console.log("리로드!!!");
-                            }}
-                        >
-                            수락
-                        </button>
-                    </>
-                )}
-            </div>
-        </div>
-    );
-}
-
-const statusCode = {
-    accept: 1,
-    refuse: 0
-};
 
 export async function fetchSubscribeList() {
     const fetchData = await getKidInfo("driver/subscribe/list");
